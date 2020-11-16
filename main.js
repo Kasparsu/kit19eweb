9dd059aa206db76393b1ff59aa77bade51fe43f7
Kaspar Suursalu <kasparsu@gmail.com>
2020-11-16 18:03:34 +0200

    lesson files

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("Hello file");
+
+// var someNameHere = "hello";
+let someNameHere = "hello";
+
+let number = 10;
+let string = "hello";
+let notChar = 'a';
+let boolean = true;
+let alsoBoolean = false;
+let isNotDefined = undefined;
+let noValue = null;
+let array = [
+    "value1",
+    "value2",
+    1,
+    true,
+    null,
+    [
+        "someother list",
+        "again"
+    ]
+];
+let object = {
+    name: "Kaspar",
+    age: 27,
+    isMale: true
+}
+console.log(object);
