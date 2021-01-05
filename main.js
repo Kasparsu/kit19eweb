aac0de220fb20c4835ad10a5e59ee7948eb9825b
Kaspar Suursalu <kasparsu@gmail.com>
2021-01-05 14:57:28 +0200

    objects, classes

--- main.js
+++ main.js
@@ -1,36 +1,64 @@
 let person = {
     name: 'Kaspar',
     age: 28,
+    print() {
+        console.log("Kaspar is 28 years old");
+    }
 };
 
-function hello(name='Nimetu'){
-    console.log("hello " + name);
-}
 
-hello();
-hello('Kaspar');
-hello('Martin');
-hello('Vello');
+person.print();
 
-function sum(a=0, b=0) {
-    if(a>0){
-        return a + b;
-    }
-    return a - b;
-}
 
-console.log(sum(4,6));
-let num = sum(7,1);
-
-if(sum(4,7) > 2) {
-    console.log("suurem");
-}
-
-function recursion(n){
-    console.log(n);
-    console.log(sum(n,n));
-    if(n>0){
-        recursion(n-1);
+let box = {
+    height: 12,
+    width: 12,
+    length: 12,
+    color: 'Red',
+    resize(height, width, length){
+        this.height = height;
+        this.width = width;
+        this.length = length;
     }
 }
-recursion(10);
+
+console.log(box);
+box.resize(10,10,8);
+console.log(box);
+box.height = 44;
+console.log(box);
+
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    print(){
+        process.stdout.write(this.name + ' is ' + this.age + ' years old');
+    }
+}
+
+let me = new Person('Kaspar', 28);
+console.log(me);
+me.print();
+let vello = new Person('Vello', 58);
+console.log(vello);
+vello.print();
+console.log(me);
+
+class Employee extends Person {
+    constructor(name, age, job){
+        super(name, age);
+        this.job = job;
+    }
+
+    print(){
+        super.print();
+        process.stdout.write('and is a ' + this.job);
+    }
+}
+
+let pets = new Employee('Pets', 44, 'Janitor');
+console.log(pets);
+pets.print();
