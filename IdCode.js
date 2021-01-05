89a654a2db1a7058287a21eb4f09662dbf4d700b
Kaspar Suursalu <kasparsu@gmail.com>
2021-01-05 16:01:47 +0200

    modules, classes, npm

---
+++ IdCode.js
@@ -0,0 +1,22 @@
+module.exports = class IdCode {
+    constructor(idCode){
+        this.idCode = idCode;
+    }
+    
+    isMale(){
+        let num = parseInt(this.idCode[0]);
+        return num % 2 === 1;
+    }
+    isFemale(){
+        return !this.isMale();
+    }
+    getCentury(){
+        let num = parseInt(this.idCode[0]);
+        num = Math.ceil(num/2);
+        return 1700 + num*100;
+    }
+    getYear(){
+        let num = parseInt(this.idCode.substr(1,2));
+        return this.getCentury() + num;
+    }
+} 
--- main.js
+++ main.js
@@ -1,64 +1,12 @@
-let person = {
-    name: 'Kaspar',
-    age: 28,
-    print() {
-        console.log("Kaspar is 28 years old");
-    }
-};
+const IdCode = require('./IdCode.js');
+const boxen = require('boxen');
 
 
-person.print();
 
+let me = new IdCode('39303217010');
+console.log( me.isMale() );
+console.log( me.isFemale() );
+console.log( me.getCentury() );
+console.log( me.getYear() );
 
-let box = {
-    height: 12,
-    width: 12,
-    length: 12,
-    color: 'Red',
-    resize(height, width, length){
-        this.height = height;
-        this.width = width;
-        this.length = length;
-    }
-}
-
-console.log(box);
-box.resize(10,10,8);
-console.log(box);
-box.height = 44;
-console.log(box);
-
-class Person {
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
-
-    print(){
-        process.stdout.write(this.name + ' is ' + this.age + ' years old');
-    }
-}
-
-let me = new Person('Kaspar', 28);
-console.log(me);
-me.print();
-let vello = new Person('Vello', 58);
-console.log(vello);
-vello.print();
-console.log(me);
-
-class Employee extends Person {
-    constructor(name, age, job){
-        super(name, age);
-        this.job = job;
-    }
-
-    print(){
-        super.print();
-        process.stdout.write('and is a ' + this.job);
-    }
-}
-
-let pets = new Employee('Pets', 44, 'Janitor');
-console.log(pets);
-pets.print();
+console.log(boxen( '' + me.getYear() , {padding: 1}));
